6e0530b12a22094300a2fd1a6341b6b1ba521626
Elizabeth Hernandez Galvan <32213372+elizabeth-hernandez-galvan@users.noreply.github.com>
2023-03-09 07:08:09 -0800

    added task 1

--- final/version1/script.js
+++ final/version1/script.js
@@ -9,22 +9,41 @@
     const score = document.getElementById('score');
     const score1 = document.getElementById("score1");
     const score2 = document.getElementById("score2");
+    const player = document.getElementById("play1");
+    const computer = document.getElementById("play2");
     const actionArea = document.getElementById('actions');
     const dice = document.getElementById('dice');
     const modal = document.getElementById("myModal");
     const myBtn = document.getElementById("learn");
     const span = document.getElementsByClassName("close")[0];
-
+    const popup = document.getElementById("popup");
     const gameData = {
             dice: ["images/1.png", "images/2.png", "images/3.png", "images/4.png", "images/5.png", "images/6.png"],
-            players: ['Player 1', 'Player 2'],
+            players: ['Player 1', 'Player 2', 'Player 3', 'Player 4','Computer'],
             score: [0, 0],
             roll1: 0,
             roll2: 0,
             rollSum: 0,
             index: 0,
-            gameEnd: 29
+            task1: 9,
+            task2: 19,
+            task3: 29
         };
+    
+        const pics = [
+            'url"images/AI_Gryffindor.jpg")',
+            'url"images/AI_Maze.jpg")',
+            'url"images/AI_Merperson.jpg")',
+            'url"images/AI_Gryffindor.jpg")'
+        ];
+
+        const pic = document.querySelector('#player');
+  
+        function showImage() {
+            let a = Math.floor(Math.random() * pics.length);
+            let img = pics[a];
+            pic.style.backgroundImage = img;
+        }
 
     const colors = ["red", "orange", "yellow", "lightgreen", "lightblue", "violet"];
 
@@ -86,10 +105,42 @@
                 modal.style.display = "none";
             }
         }
+
+        function choosePlayer() {
+            // Random Player Generator
+        }
         
+        // Function to Play Game (Start of play vs Computer)
+        const playGame = () => {
+        const fightBtn = document.querySelector('.rollagain');
+        const passBtn = document.querySelector('.pass');
+        // const playerOpt = [fightBtn,passBtn];
+        const computerOpt = ['fight','pass'];
+        
+            // Function to start playing game
+            playerOptions.forEach(option => {
+                option.addEventListener('click',function(){
+        
+                    const choiceNumber = Math.floor(Math.random()*2); //(2 chocies)
+                    const computerChoice = computerOpt[choiceNumber];
+        
+                    // Function to check who wins
+                    winner(this.innerText,computerChoice)
+                    
+                    // if(// Calling gameOver function){
+                
+                    // }
+                })
+            })
+        }
+        
+
         startGame.addEventListener('click', function() {
             document.querySelector("#landing").className = "hidden";
             gameControl.className = "showing";
+
+            showImage();
+
             //randomy set game index here
             gameData.index = Math.round(Math.random());
             console.log(`index: ${gameData.index}`);
@@ -104,7 +155,7 @@
 
         function setUpTurn() {
             game.className = "showing";
-            game.innerHTML = `<p class = "text">${gameData.players[gameData.index]}</p>`;
+            game.innerHTML = `<p class = "text">${gameData.players[gameData.index]}'s Turn</p>`;
             actionArea.innerHTML = '<button id = "roll"> Fight </button>';
 
             document.getElementById('roll').addEventListener("click", function(){
@@ -178,20 +229,63 @@
         }
 
         //function to check for winner
-        function checkWinningCondition(){
-            if (gameData.score[gameData.index] > gameData.gameEnd) {
-
-                console.log("WIN")
+        function checkWinningCondition(player,computer){
+            if (gameData.score[gameData.index] > gameData.task1) {
+                console.log("Task1")
                 
                 showCurrentScore();
 
-                score.innerHTML += `<h2 class="winner">${gameData.players[gameData.index]} wins with ${gameData.score[gameData.index]} points!</h2>`;
+                if(player === gameData.score[gameData.index] > gameData.task1){
+                    console.log("PLAYER WINS")
+                    showCurrentScore();
+
+                    score.innerHTML += `<h2 class="winner">Good work, you win with ${gameData.score[gameData.index]} points!</h2>`;
                 
-                actionArea.innerHTML = '';
-                document.getElementById('quit').innerHTML = "Start a New Game?";
+                    actionArea.innerHTML = '';
+                    document.getElementById('quit').innerHTML = "Start a New Game?";
+                }
+                else if(computer == gameData.score[gameData.index] > gameData.task1){
+                    console.log("COMPUTER WINS")
+                    score.innerHTML += `<h2 class="winner">Sorry, the computer wins with ${gameData.score[gameData.index]} points!</h2>`;
+                
+                    actionArea.innerHTML = '';
+                    document.getElementById('quit').innerHTML = "Start a New Game?";
+            
+                    }
+
+                // score.innerHTML += `<h2 class="winner">${gameData.players[gameData.index]} wins with ${gameData.score[gameData.index]} points!</h2>`;
+                
+                // actionArea.innerHTML = '';
+                // document.getElementById('quit').innerHTML = "Start a New Game?";
+
+                openPopup()
 
                 return true;
+            }else if (gameData.score[gameData.index] > gameData.task2) {
+                console.log("Task2")
+                
+                showCurrentScore();
 
+                if(player === gameData.score[gameData.index] > gameData.task1){
+                    console.log("PLAYER WINS")
+                    showCurrentScore();
+
+                    score.innerHTML += `<h2 class="winner">Good work, you win with ${gameData.score[gameData.index]} points!</h2>`;
+                
+                    actionArea.innerHTML = '';
+                }
+                else if(computer == gameData.score[gameData.index] > gameData.task1){
+                    console.log("COMPUTER WINS")
+                    score.innerHTML += `<h2 class="winner">Sorry, the computer wins with ${gameData.score[gameData.index]} points!</h2>`;
+                
+                    actionArea.innerHTML = '';
+                    document.getElementById('quit').innerHTML = "Start a New Game?";
+            
+                    }
+
+                openPopup()
+
+                return true;
             } else {
                 showCurrentScore();
                 return false;
@@ -202,6 +296,25 @@
         function showCurrentScore(){
             score1.innerHTML = `Score: ${gameData.score[0]}`;
             score2.innerHTML = `Score: ${gameData.score[1]}`;
+        }
+
+        //Turn on Overlay
+        function openPopup() {
+        popup.classList.add("open")
+        popup.innerHTML = '<h1>CONGRATULATIONS</h1><div class="imgRow"><img src="images/AI_dragon.jpg" alt="dragon"><img src="images/AI_Merperson.jpg" alt="merperson"><img src="images/AI_Maze.jpg" alt="maze"></div><h2>The dragon has been slayed. You have passed the first challenge! Next Challenge: Surviving the Hogwarts Lake.</h2><button id = "continue"> Continue </button>';
+        console.log("overlay on");
+        }
+
+        const cont = document.getElementById("continue");
+
+        cont.onclick = function() {
+            closePopup();
+            throwDice();
+        }
+
+        function closePopup() {
+        popup.classList.remove("open");
+        console.log("overlay off");
         }
 
 }())
